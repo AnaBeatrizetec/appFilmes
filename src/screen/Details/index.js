d9687f85a3d98fd6a250ef073a3114921f4d9e0c
ana <ana.santos2729@etec.sp.gov.br>
2023-09-22 17:29:01 -0300

    first commit

---
+++ src/screen/Details/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {View, Text, Image} from 'react-native'
+import { useRoute } from '@react-navigation/native'
+import styles from './style';
+
+
+export default function Details(){
+
+
+    const routes = useRoute();
+    return(
+        <View>
+         
+            <Image style={styles.images} source = {require (`../../Img/${routes.params.imagem}`)} />
+            <Text> {routes.params.titulo} </Text>
+            <Text style={styles.textNota}>{routes.params.nota}</Text>
+        </View>
+    )
+}
---
+++ src/screen/Details/style.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+
+  
+
+    titulo:{
+        color: "#fff",
+        fontSize: 15,
+        paddingTop: 8
+    },
+  
+    images:{
+        width: '100%',
+        height: 600,
+        borderRadius:8
+    },
+
+    textNota:{
+        fontSize: 15,
+        color: "black",
+        paddingLeft: 4
+    }
+
+
+})
+
+export default styles;
